3d7f2a632c988e5094c080ee51540115056352d6
Colton-Windsor <coltonblakewindsor@gmail.com>
2024-07-10 19:00:04 +0000

    Problem 11 Solve

--- 01_restaurant/challenges/01_11_trackfavorites_ch.sql
+++ 01_restaurant/challenges/01_11_trackfavorites_ch.sql
@@ -1,4 +1,21 @@
 -- Update information in the database.
 
 -- Set Cleo Goldwater's favorite dish to
--- the Quinoa Salmon Salad.
+-- the Quinoa Salmon Salad. unique ID 9
+
+SELECT DishID
+FROM Dishes
+WHERE Name = 'Quinoa Salmon Salad'
+
+SELECT *
+FROM Customers
+WHERE FirstName = 'Cleo' AND LastName = 'Goldwater'
+
+UPDATE Customers
+SET FavoriteDish = 9
+WHERE CustomerID = 42;
+
+SELECT Customers.FirstName, Customers.LastName, Customers.FavoriteDish, Dishes.Name
+FROM Customers
+JOIN Dishes ON Customers.FavoriteDish = Dishes.DishID
+WHERE Customers.FirstName = 'Cleo' AND Customers.LastName = 'Goldwater';
