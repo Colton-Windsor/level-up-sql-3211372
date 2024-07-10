46ce3fef63a7a2ab382c1c23061f94a0e0da758d
Colton-Windsor <coltonblakewindsor@gmail.com>
2024-07-10 15:43:45 +0000

    working on 9

--- 01_restaurant/challenges/01_09_createreservation_ch.sql
+++ 01_restaurant/challenges/01_09_createreservation_ch.sql
@@ -4,3 +4,7 @@
 -- Use the following information:
 -- Sam McAdams (smac@kinetecoinc.com), for 5 people
 -- on August 12, 2022 at 6PM (18:00)
+
+SELECT *
+FROM Customers
+WHERE Email = 'smac@kinetecoinc.com';
