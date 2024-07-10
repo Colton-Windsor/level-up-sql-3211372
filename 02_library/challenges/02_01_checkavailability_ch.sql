e4648aed481bdf3d2a7923c58a67bed0d8ffa925
Colton-Windsor <coltonblakewindsor@gmail.com>
2024-07-10 19:54:00 +0000

    2-1 Solve

--- 02_library/challenges/02_01_checkavailability_ch.sql
+++ 02_library/challenges/02_01_checkavailability_ch.sql
@@ -1,2 +1,21 @@
 -- Determine how many copies of the book 'Dracula'
 -- are available for library patrons to borrow.
+
+SELECT COUNT(Books.Title)
+FROM Books
+WHERE Books.Title = 'Dracula';
+
+SELECT COUNT(Books.Title)
+FROM Loans
+JOIN Books ON Loans.BookID = Books.BookID
+WHERE Loans.ReturnedDate IS NULL AND Books.Title = 'Dracula';
+
+SELECT 
+(SELECT COUNT(Books.Title)
+FROM Books
+WHERE Books.Title = 'Dracula') -
+(SELECT COUNT(Books.Title)
+FROM Loans
+JOIN Books ON Loans.BookID = Books.BookID
+WHERE Loans.ReturnedDate IS NULL AND Books.Title = 'Dracula')
+AS 'Total Available Copies of Dracula';
