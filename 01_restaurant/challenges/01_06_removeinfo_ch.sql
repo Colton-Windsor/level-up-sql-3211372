303bb62f0a78d2dbf587cb905b71a9f32cb5d837
Colton-Windsor <coltonblakewindsor@gmail.com>
2024-07-10 14:55:47 +0000

    problem 6 solve

--- 01_restaurant/challenges/01_06_removeinfo_ch.sql
+++ 01_restaurant/challenges/01_06_removeinfo_ch.sql
@@ -4,5 +4,26 @@
 -- be able to keep his Friday reservation. 
 -- Today is July 24, 2022.
 
-DELETE FROM Customers 
-WHERE CustomerID = 102
+SELECT 
+  CustomerID,
+  FirstName,
+  LastName
+FROM Customers
+WHERE FirstName = 'Norby';
+
+SELECT * FROM Reservations
+WHERE CustomerID = 64
+ORDER BY Date;
+
+DELETE FROM Reservations 
+WHERE ReservationID = 2000;
+
+-- Above was my solution - They're intention was for me to join 
+-- the customer and reservation tables first, so below is my attempt at that. 
+
+SELECT * FROM Reservations
+JOIN Customers ON Reservations.CustomerID = Customers.CustomerID
+WHERE Customers.FirstName = 'Norby' AND Reservations.Date > '2022-07-24';
+
+DELETE FROM Reservations 
+WHERE ReservationID = 2000;
