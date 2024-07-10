506c7955b07e6bb2d6ca6d1ff72955fd8ba4a592
Colton-Windsor <coltonblakewindsor@gmail.com>
2024-07-10 20:38:31 +0000

    2-3 Solved

--- 02_library/challenges/02_03_checkout_ch.sql
+++ 02_library/challenges/02_03_checkout_ch.sql
@@ -1,5 +1,29 @@
--- Check out two books for Jack Vaan (jvaan@wisdompets.com).
+-- Check out two books for Jack Vaan (jvaan@wisdompets.com). patron ID 50
 -- Book 1: The Picture of Dorian Gray, 2855934983
 -- Book 2: Great Expectations, 4043822646
 -- The checkout date is August 25, 2022 and the 
 -- due date is September 8, 2022.
+
+SELECT PatronID, FirstName, LastName, Email
+FROM Patrons
+WHERE FirstName = 'Jack' AND LastName = 'Vaan';
+
+INSERT INTO Loans (BookID, PatronID, LoanDate, DueDate)
+VALUES (
+  (SELECT BookID FROM Books WHERE Barcode = '2855934983'),
+  (SELECT PatronID FROM Patrons WHERE Email = 'jvaan@wisdompets.com'),
+  '2022-08-25',
+  '2022-09-08');
+
+INSERT INTO Loans (BookID, PatronID, LoanDate, DueDate)
+VALUES (
+  (SELECT BookID FROM Books WHERE Barcode = '4043822646'),
+  (SELECT PatronID FROM Patrons WHERE Email = 'jvaan@wisdompets.com'),
+  '2022-08-25',
+  '2022-09-08');
+
+SELECT *
+FROM Loans
+JOIN Books ON Loans.BookID = Books.BookID
+WHERE PatronID = (SELECT PatronID FROM Patrons WHERE Email = 'jvaan@wisdompets.com')
+ORDER BY LoanDate DESC;
